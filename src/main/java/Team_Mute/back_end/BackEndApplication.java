cfdecb4461d5e5448ed804963803cc463044805c
Heebeom Song <songh6508@naver.com>
2025-08-20 14:17:50 +0900

    Fix: 스웨거 CORS 에러 수정

--- src/main/java/Team_Mute/back_end/BackEndApplication.java
+++ src/main/java/Team_Mute/back_end/BackEndApplication.java
@@ -3,6 +3,11 @@ package Team_Mute.back_end;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {
+	@Server(url = "https://healthy-velvet-sinhan-space-rental-36c4aa0c.koyeb.app", description = "Default Server URL")})
 @SpringBootApplication
 public class BackEndApplication {
 
